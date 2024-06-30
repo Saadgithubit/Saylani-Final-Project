a8e5ef06e322b1599f267640b16b3a4768e8a821
Saad Ahmed <137560242+Saadgithubit@users.noreply.github.com>
2024-06-30 22:27:04 +0500

    changes in sign up page

--- src/view/Signup/index.jsx
+++ src/view/Signup/index.jsx
@@ -84,9 +84,8 @@ export default function Signup() {
                                 <MenuItem value="">
                                     <em>None</em>
                                 </MenuItem>
-                                <MenuItem value={10}>Ten</MenuItem>
-                                <MenuItem value={20}>Twenty</MenuItem>
-                                <MenuItem value={30}>Thirty</MenuItem>
+                                <MenuItem value={10}>Teacher</MenuItem>
+                                <MenuItem value={20}>Student</MenuItem>
                             </Select>
                             <FormHelperText sx={{color: '#376ABE'}}>Are You Student Or Teacher?</FormHelperText>
                         </FormControl>
