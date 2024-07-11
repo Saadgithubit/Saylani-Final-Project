11019b3e4db62dc7c725d937c9f4157f734434ad
Muneeb-Babar <muneeb63ahmad63@gmail.com>
2024-07-11 15:25:24 +0500

    Render Assignment Data

--- src/Components/Assignment/index.jsx
+++ src/Components/Assignment/index.jsx
@@ -1,23 +1,137 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { getTeacherAllAsigmment } from "../../Config/mongodbmain";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 export default function Assignment() {
-const [data,serData] = useState([])
-
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-       
-        fetcData()
-      }, []);
+        fetchData();
+    }, []);
 
+    const fetchData = async () => {
+        const res = await getTeacherAllAsigmment();
+        setData(res.data);
+        console.log(res.data);
+    };
 
-      const fetcData = async() => {
-      const data =  await getTeacherAllAsigmment()
-       } 
+    function formatDateTime(dateTimeString) {
+        const options = {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit'
+        };
+        return new Date(dateTimeString).toLocaleDateString(undefined, options);
+    }
 
+    function Row(props) {
+        const { row } = props;
+        const [open, setOpen] = React.useState(false);
 
+        return (
+            <React.Fragment>
+                <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+                    <TableCell>
+                        <IconButton
+                            aria-label="expand row"
+                            size="small"
+                            onClick={() => setOpen(!open)}
+                        >
+                            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                        </IconButton>
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                        {row.title}
+                    </TableCell>
+                    <TableCell align="right">{row.description}</TableCell>
+                    <TableCell align="right">{formatDateTime(row.dueDate)}</TableCell>
+                    <TableCell align="center">
+                        <img src={row.content} alt={row.title} width="100" />
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            <Box sx={{ margin: 1 }}>
+                                <Typography variant="h6" gutterBottom component="div">
+                                    History
+                                </Typography>
+                                <Table size="small" aria-label="purchases">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>Date</TableCell>
+                                            <TableCell align="right">Time</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {row.history && row.history.map((historyRow) => (
+                                            <TableRow key={historyRow.date}>
+                                                <TableCell component="th" scope="row">
+                                                    {new Date(historyRow.date).toLocaleDateString()}
+                                                </TableCell>
+                                                <TableCell align="right">
+                                                    {new Date(historyRow.date).toLocaleTimeString()}
+                                                </TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </Box>
+                        </Collapse>
+                    </TableCell>
+                </TableRow>
+            </React.Fragment>
+        );
+    }
+
+    Row.propTypes = {
+        row: PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+            dueDate: PropTypes.string.isRequired,
+            content: PropTypes.string.isRequired,
+            history: PropTypes.arrayOf(
+                PropTypes.shape({
+                    date: PropTypes.string.isRequired,
+                }),
+            ),
+        }).isRequired,
+    };
 
     return (
-        <div>Assignment</div>
-    )
+        <TableContainer component={Paper}>
+            <Table aria-label="collapsible table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell />
+                        <TableCell>Title</TableCell>
+                        <TableCell align="right">Description</TableCell>
+                        <TableCell align="right">Due Date</TableCell>
+                        <TableCell align="right">Content</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.map((row) => (
+                        <Row key={row.id} row={row} />
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
